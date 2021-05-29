362c2164946305f9f0ec6e2cc2c7d5e246ced8f2
sousacaio <frias.caio@yahoo.com.br>
2021-05-29 17:43:40 -0300

    chore: add postgres configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, //tirar essa option depois
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
