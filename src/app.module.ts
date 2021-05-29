9b20542141fc315dcccbf0930a6d1e9506a1345a
sousacaio <frias.caio@yahoo.com.br>
2021-05-29 17:55:47 -0300

    feat: add user module and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
       autoLoadEntities: true,
       synchronize: true, //tirar essa option depois
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
