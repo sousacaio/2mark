8045be7394b6dd803efe49a70a2027eb4770e587
sousacaio <frias.caio@yahoo.com.br>
2021-05-30 12:23:31 -0300

    test: ensure findOne method is called with correct params

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,3 +1,4 @@
+import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
@@ -17,7 +18,14 @@ describe('UsersController', () => {
       email: 'valid_email@mail.com.br',
       password: 'valid_password',
     },
-  ];
+  ] as User[];
+  const user = {
+    id: 0,
+    name: 'valid_name',
+    email: 'valid_email@mail.com.br',
+    password: 'valid_password',
+  } as User;
+
   beforeEach(async () => {
     userService = new UsersService();
     userController = new UsersController(userService);
@@ -34,11 +42,21 @@ describe('UsersController', () => {
     });
   });
   it('should call findAll method', async () => {
-    const result = new Promise((resolve) => resolve(userExample));
+    const result = new Promise<User[]>((resolve) => resolve(userExample));
     const spy = jest.spyOn(userService, 'findAll').mockReturnValue(result);
     const response = await userController.findAll();
     expect(response).not.toBeNull();
     expect(spy).toHaveBeenCalled();
     expect(spy).toHaveBeenCalledTimes(1);
+  });
+  it('should call findOne method', async () => {
+    const spy = jest
+      .spyOn(userService, 'findOne')
+      .mockReturnValue(new Promise<User>((resolve) => resolve(user)));
+    const response = await userController.findOne(0);
+    expect(response).not.toBeNull();
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(0);
   });
 });
