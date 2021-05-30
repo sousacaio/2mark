653ed7174d9e2803236278567ea24bcaee4a9973
sousacaio <frias.caio@yahoo.com.br>
2021-05-29 23:52:50 -0300

    feat: add updatecart

--- src/cart/dto/update-cart.dto.ts
+++ src/cart/dto/update-cart.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsInt, IsNotEmpty, IsOptional } from 'class-validator';
 import { CreateCartDto } from './create-cart.dto';
 
-export class UpdateCartDto extends PartialType(CreateCartDto) {}
+export class UpdateCartDto extends PartialType(CreateCartDto) {
+  @IsInt()
+  @IsOptional()
+  id: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  quantity: number;
+}
